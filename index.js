0f91ec67c8304b16d703eb005c2a6d4abe3e2566
julesbroeks <julesbroeks@gmail.com>
2021-12-02 13:51:49 +0100

    Start-kit-nodejs

---
+++ index.js
@@ -0,0 +1,17 @@
+// config //
+let overrideDay = 3; // set to number to overwrite default (today)
+
+// imports //
+let fs = require('fs');
+let logic = require("./logic/day-" + overrideDay ?? new Date().getDate());
+
+
+let input = null;
+let filename = "input/day-" + (overrideDay ?? new Date().getDate()) + ".txt";
+
+fs.readFile(filename, 'utf8', function(err, data) {
+  if (err) throw err;
+  input = data;
+  console.log('Succesfully loaded: ' + filename);
+  logic.start(input, overrideDay ?? new Date().getDate());
+});
---
+++ logic/day-20.js
@@ -0,0 +1,16 @@
+module.exports = {
+  start: function (input, day) {
+    console.log("Starting day " + day + " - part 1")
+    // part 1 code
+
+    //
+    finish(input, day);
+  },
+}
+let finish = (input, day) =>  {
+  // part 2 code
+
+  //
+  console.log("Starting day " + day + " - part 2")
+};
+
